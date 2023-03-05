39e8aff4a59046f27ab6897ae7d8dd853a6b631b
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-03-05 17:33:31 +0300

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -16,7 +16,7 @@ function Business() {
     <section id='services' className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
       <div className={layout.sectionInfo}>
         <h2 className={`${styles.heading2} text-gradient`}>
-        Revolutionize Your Cancer Screening with <br className='sm:block hidden'/> Cutting-Edge Digital Solutions from [AI Startup Name]
+        Revolutionize Your Cancer Screening with <br className='sm:block hidden'/> Cutting-Edge Digital Solutions from <span className='text-white'>Saratani</span><span className='text-gradient'>Ai</span>
         </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>At <span className='text-gradient font-poppins font-bold'>Saratani</span><span className='text-white'>Ai</span>, we offer a range of digital cancer screening and diagnosis services. Our solutions are developed using scientific research, and are designed to be accurate, reliable, and cost-effective. Some of our services include:</p>
         
