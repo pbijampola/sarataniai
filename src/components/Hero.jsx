67b59b7e9a7b343ba4d0302312bd3e88451b086c
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-03-05 18:36:11 +0300

    Modified Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,13 @@ const Hero=() =>
  (
     <section id='home' className={`flex md:flex-row flex-col ${Styles.paddingY}`}>
       <div className={`flex-1 ${Styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className={`flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2`}>
+        {/* <div className={`flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2`}>
           <img src={discount} alt=""  className='w-[32px] h-[32]'/>
          <p className={`${Styles.paragraph}`}>
          <span className='text-white'>20%</span>Discount for {""}
           <span className='text-white'>Month</span> Account
          </p>
-        </div>
+        </div> */}
         <div className='flex flex-row justify-between items-center w-full'>
 
           <h1 className='flex-1 font-poppins font-semibold ss:text-[52px] text-[32px] text-white ss:leading-[100px] leading-[75px]'>
