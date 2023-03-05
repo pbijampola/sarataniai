31ff45738cccccb013e65949a332223f66edc39c
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-03-05 18:27:58 +0300

    Update style.js

--- src/style.js
+++ src/style.js
@@ -1,7 +1,7 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
-    heading2: "font-poppins font-semibold xs:text-[38px] text-[30px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+    heading2: "font-poppins font-semibold xs:text-[24px] text-[30px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
     paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
   
     flexCenter: "flex justify-center items-center",
@@ -16,7 +16,7 @@ const styles = {
   };
   
   export const layout = {
-    section: `flex md:flex-row flex-col ${styles.paddingY}`,
+    section: `flex md:flex-row flex-col ${styles.paddingX}`,
     sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
   
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
