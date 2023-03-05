ed119b0e234780c0b2459cedca6e23b6a2069e73
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-03-05 13:48:45 +0300

    Hero section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -78,17 +78,17 @@ export const stats = [
   {
     id: "stats-1",
     title: "User Active",
-    value: "3800+",
+    value: "800+",
   },
   {
     id: "stats-2",
-    title: "Trusted by Company",
-    value: "230+",
+    title: "Trusted by Hospitals",
+    value: "150+",
   },
   {
     id: "stats-3",
-    title: "Transaction",
-    value: "$230M+",
+    title: "Diagnosis",
+    value: "2300+",
   },
 ];
 
